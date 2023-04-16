0e263e94de54087dd3777453883540a79fa90589
IsaiahGz <isaiahgala99@gmail.com>
2023-04-16 12:33:08 -0400

    Model associations

--- models/index.js
+++ models/index.js
@@ -4,17 +4,36 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 // Categories have many Products
+Category.hasMany(Product, {
+	foreignKey: 'category_id',
+	onDelete: 'CASCADE',
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+	foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+	through: {
+		model: 'producttag',
+		unique: false,
+	},
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+	through: {
+		model: 'producttag',
+		unique: false,
+	},
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+	Product,
+	Category,
+	Tag,
+	ProductTag,
 };
