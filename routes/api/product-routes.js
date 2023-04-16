762701b5916929a7bb23a2003524015f938e2c28
IsaiahGz <isaiahgala99@gmail.com>
2023-04-16 18:17:05 -0400

    Delete by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -100,8 +100,18 @@ router.put('/:id', (req, res) => {
 		});
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
 	// delete one product by its `id` value
+	try {
+		const amtDeleted = await Product.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
+		res.status(200).json({ message: 'Product deleted', amount: amtDeleted });
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
